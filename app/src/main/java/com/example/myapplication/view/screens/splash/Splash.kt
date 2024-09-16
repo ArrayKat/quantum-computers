93f2403a52e82b78179a552a040347afc9d0fd7f
ArrayKat <kolinichenko.05@mail.ru>
2024-09-17 01:19:14 +0300

    finish splashScreen

--- app/src/main/java/com/example/myapplication/view/screens/splash/Splash.kt
+++ app/src/main/java/com/example/myapplication/view/screens/splash/Splash.kt
@@ -1,48 +1,70 @@
 package com.example.myapplication.view.screens.splash
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.myapplication.R
 import kotlinx.coroutines.delay
 
 @Composable
 fun Splash(navHostController: NavHostController) {
+
+
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(color = MaterialTheme.colorScheme.primary)
     ) {
-        Text(
-            text = "Нажми на меня",
-            modifier = Modifier
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo",
+                modifier = Modifier.size(150.dp)
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            Text(
+                text = "Quantum Computers",
+                color = Color.White,
+                fontSize = 55.sp,
+                fontWeight = FontWeight.Bold,
+                lineHeight = 1.em,
+                textAlign = TextAlign.Center
+            )
 
-
-
-
-
-
-
-
-
-//            modifier = Modifier.clickable {
-//                navHostController.navigate("signIn"){
-//                    popUpTo("splash"){ //заканчиваем жизненный цикл экрана сплэш
-//                        inclusive = true
-//                    }
-//                }
-//            }.padding(30.dp)
-
-        )
+        }
     }
+
+
+
+
+
+
     LaunchedEffect(true) {
         delay(5000)
         navHostController.navigate("signIn") {
